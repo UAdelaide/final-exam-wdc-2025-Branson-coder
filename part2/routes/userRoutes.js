aa858a65e33210dc1d453f1c1c455a8bad6bd3fe
Branson Khoo <a1925025@adelaide.edu.au>
2025-06-20 06:51:55 +0000

    [automatic commit] 2025-06-20 06:51:55 UTC 119.15.75.26

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -56,7 +56,10 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/api/login', async(req, res) =>{
-  const { username, password}
+  const { username, password } = req.body;
+  try{
+    const [users]
+  }
 });
 
 module.exports = router;
