abcfe051741783c263964a974a138de7e3758cb8
Branson Khoo <a1925025@adelaide.edu.au>
2025-06-20 07:05:52 +0000

    [automatic commit] 2025-06-20 07:05:52 UTC 119.15.75.26

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -57,12 +57,13 @@ router.post('/login', async (req, res) => {
 
 router.post('/api/login', async(req, res) => {
   const { username, password } = req.body;
+  console.log('Query result:', rows);
   try{
     const [users] = await db.execute(
       `SELECT * FROM Users WHERE username = ? AND password_hash = ?`,
       [username, password]
     );
-    if(users.length == 1){
+    if(users.length === 1){
       const user = users[0];
       req.session.user = {
         id: user.user_id,
