d7839fef5ed26015412c16df6fcdf06f3e22da50
Branson Khoo <a1925025@adelaide.edu.au>
2025-06-20 03:02:11 +0000

    [automatic commit] 2025-06-20 03:02:11 UTC 119.15.75.26

--- part1/app.js
+++ part1/app.js
@@ -26,7 +26,7 @@ let db;
         await db.execute(`
             INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
             ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-            
+            ('bobwalker', 'bob@example.com)
         `)
     }
 })
