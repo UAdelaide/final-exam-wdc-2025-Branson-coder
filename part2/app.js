e86b71301c4d765ad2e261f11be378495235f7cf
Branson Khoo <a1925025@adelaide.edu.au>
2025-06-20 07:18:38 +0000

    [automatic commit] 2025-06-20 07:18:38 UTC 119.15.75.26

--- part2/app.js
+++ part2/app.js
@@ -15,6 +15,6 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.use('/api', userRouter);
+app.use('/api', userRoutes);
 // Export the app instead of listening here
 module.exports = app;
