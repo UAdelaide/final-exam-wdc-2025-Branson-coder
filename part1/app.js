739f1329f5e035762880196e59839c486204d681
Branson Khoo <a1925025@adelaide.edu.au>
2025-06-20 02:42:57 +0000

    [automatic commit] 2025-06-20 02:42:57 UTC 119.15.75.26

--- part1/app.js
+++ part1/app.js
@@ -15,6 +15,8 @@ let db;
 
 async function initialiseDB(){
     try{
-        var connection = await mysql.createConnection
+        var connection = await mysql.createConnection({
+            
+        })
     }
 }
