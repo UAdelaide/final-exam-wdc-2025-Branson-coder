c9a40b33dbd39bf3310b64f0e0332e477e56d4c0
Branson Khoo <a1925025@adelaide.edu.au>
2025-06-20 02:55:07 +0000

    [automatic commit] 2025-06-20 02:55:07 UTC 119.15.75.26

--- part1/app.js
+++ part1/app.js
@@ -1,13 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+
+const app = express();
+app.use(express.json());
+
+let db;
+
 (async () => {
   try {
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: 'HoangPoochie',
+      password: 'test',
       database: 'DogWalkService'
     });
 
-    // Sample data insertion (ignores duplicates)
     await db.execute(`INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
       ('alice123',    'alice@example.com',     'hashed123', 'owner'),
       ('bobwalker',   'bob@example.com',       'hashed456', 'walker'),
