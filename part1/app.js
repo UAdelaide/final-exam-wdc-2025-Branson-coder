371173eb0a7b14a6f3879f2d2c343aa359a35723
Branson Khoo <a1925025@adelaide.edu.au>
2025-06-20 04:44:01 +0000

    [automatic commit] 2025-06-20 04:44:01 UTC 119.15.75.26

--- part1/app.js
+++ part1/app.js
@@ -9,7 +9,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(express.static('public'));
 let db;
 
 (async () => {
@@ -136,6 +136,6 @@ app.get('/api/walkers/summary', async function(req, res) {
     res.status(500).json({ error: 'Failed to fetch walker summary' });
   }
 });
-app.use(express.static('public'));
+
 module.exports = app;
 
